3a206fe49e49d3e2c6ac65458a8fb0a6add3c3d7
Sergey <sereganiceman@gmail.com>
2022-01-01 22:06:09 +0200

    fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    ignorePatterns: [".eslintrc.js", "*.html"],
-    extends: ["plugin:sonarjs/recommended", "metarhia"],
-    plugins: ["sonarjs"],
-    rules: {
-        quotes: ["error", "double", { avoidEscape: true }],
-        "sonarjs/cognitive-complexity": ["error", 15],
-    },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  ignorePatterns: [".eslintrc.js", "*.html"],
+  extends: ["plugin:sonarjs/recommended", "metarhia"],
+  plugins: ["sonarjs"],
+  rules: {
+    quotes: ["error", "double", { avoidEscape: true }],
+    "sonarjs/cognitive-complexity": ["error", 15],
+  },
 };
